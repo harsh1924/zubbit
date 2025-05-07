6691a7ed095e465bfa20be47855997b2b4a27c73
harsh1924 <harshrpanwar@gmail.com>
2025-05-07 12:19:24 +0530

    Completed File Schema

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, uuid, integer, boolean } from "drizzle-orm/pg-core";
+import { pgTable, text, uuid, integer, boolean, timestamp } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
 export const files = pgTable("files", {
@@ -11,5 +11,39 @@ export const files = pgTable("files", {
     type: text("type").notNull(), // "Default Folder"
 
     // Storge Information
-    fileUrl: text("file_url")
+    fileUrl: text("file_url"),
+    thumbnailUrl: text("thumbnail_url"),
+
+    // Ownership
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"),
+
+    // FIle/Folder flags
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("is_trashed").default(false).notNull(),
+
+    // Timestamps
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })
+
+/*
+parent: Each file/folder can have one parent folder
+
+children: Each folder can have many child files/folder
+*/
+
+export const filesRelations = relations(files, ({ one, many }) => {
+    parent: one(files, {
+        fields: [files.parentId],
+        references: [files.id]
+    });
+
+    // Relationship to child file/folder
+    children: many(files)
+})
+
+// Type definations
+export const File = typeof files.$inferSelect
+export const NewFile = typeof files.$inferInsert
